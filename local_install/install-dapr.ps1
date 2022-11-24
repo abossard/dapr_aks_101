091cb050bfecd3e3cc3b5333bb882e92bac8f68e
Flisch Gilles <Web@flisch.be>
2022-11-24 09:04:47 +0100

    Add setup path

--- local_install/install-dapr.ps1
+++ local_install/install-dapr.ps1
@@ -7,6 +7,15 @@ if ($args.Length -eq 0)  {
 
 $Dapr_Version = $args[0]
 $InstallationDirectory = "c:\Prj\dapr"
+
+$MYPATH = Get-ItemPropertyValue HKCU:\Environment -Name Path
+if (!$MYPATH.Contains($InstallationDirectory))
+{
+    $MYPATH += ";" + $InstallationDirectory
+
+    Set-ItemProperty -Path HKCU:\Environment -Name Path -Value $MYPATH
+}
+
 $Env:DAPR_INSTALL_DIR = $InstallationDirectory
 $script=iwr -useb https://raw.githubusercontent.com/dapr/cli/master/install/install.ps1; $block=[ScriptBlock]::Create($script); invoke-command -ScriptBlock $block -ArgumentList "", "$Env:DAPR_INSTALL_DIR"
 cp .\ldapr.ps1 $InstallationDirectory
