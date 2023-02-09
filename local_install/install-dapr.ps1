f4da379b2fdd1d6f8e1dd1edc58a8ea5cb168961
Flisch Gilles <Web@flisch.be>
2023-02-09 09:27:16 +0100

    Improve installation by removing dapr bin directory if exists.

--- local_install/install-dapr.ps1
+++ local_install/install-dapr.ps1
@@ -1,12 +1,13 @@
 
 if ($args.Length -eq 0)  {
     Write-Host "Please provide the Dapr version you want to install" -foregroundColor Red -BackGroundColor Black
-    Write-Host "Version must be v.x.y.z"
+    Write-Host "Version must be vx.y.z"
     exit
 }
 
 $Dapr_Version = $args[0]
 $InstallationDirectory = "c:\Prj\dapr"
+$InstallationPostDirectory = ".dapr\bin"
 
 $MYPATH = Get-ItemPropertyValue HKCU:\Environment -Name Path
 if (!$MYPATH.Contains($InstallationDirectory))
@@ -20,7 +21,11 @@ $Env:DAPR_INSTALL_DIR = $InstallationDirectory
 $script=iwr -useb https://raw.githubusercontent.com/dapr/cli/master/install/install.ps1; $block=[ScriptBlock]::Create($script); invoke-command -ScriptBlock $block -ArgumentList "", "$Env:DAPR_INSTALL_DIR"
 cp .\ldapr.ps1 $InstallationDirectory
 cd $InstallationDirectory
-md ".dapr\bin"
+if (Test-Path $InstallationPostDirectory) 
+{ 
+    Remove-Item -Recurse -Force $InstallationPostDirectory
+}
+md $InstallationPostDirectory
 iwr -Uri "https://github.com/dapr/dapr/releases/download/$($Dapr_Version)/daprd_windows_amd64.zip" -Outfile ".\daprd_windows_amd64.zip"
 Expand-Archive daprd_windows_amd64.zip -DestinationPath .\.dapr\bin
 Remove-Item daprd_windows_amd64.zip
